82959f6b8b84b19e6349e4f20fdbf7c341f4f5ec
Jun Santilla <junsantilla@live.com>
2026-08-20 14:03:07 +0200

    add new members

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import { getCurrentUser } from "@/lib/auth";
 import Profile from "@/app/components/Profile";
 import Landing from "@/app/components/Landing";
+import NewMembers from "@/app/components/NewMembers";
 
 export default async function Home() {
-  const user = await getCurrentUser();
-  if (!user) return <Landing />;
+    const user = await getCurrentUser();
+    if (!user) return <Landing />;
 
-  return (
-    <div className="max-w-[960px] w-full mx-auto">
-      <Profile user={user} currentUser={user} />
-    </div>
-  );
+    return (
+        <div className="max-w-[960px] w-full mx-auto">
+            <Profile user={user} currentUser={user} />
+            <div className="bg-white border border-[#6699cc] sm:border-x p-2.5 mt-3">
+                <NewMembers excludeId={user._id.toString()} />
+            </div>
+        </div>
+    );
 }
